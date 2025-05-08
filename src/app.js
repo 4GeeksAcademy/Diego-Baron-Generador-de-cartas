c77c813838d075604841210100d391a722bf1b9e
Juan Diego Baron Faura <JDBF1011@Gmail.com>
2025-05-08 02:14:17 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
-import "bootstrap";
-import "./style.css";
+function generarCarta() {
+  const simbolos = ["♥", "♦", "♣", "♠"];
+  const valores = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+  const simbolo = simbolos[Math.floor(Math.random() * simbolos.length)];
+  const valor = valores[Math.floor(Math.random() * valores.length)];
+  const esRojo = simbolo === "♥" || simbolo === "♦";
+
+  const cartaHTML = `
+    <div class="esquina superior ${esRojo ? 'rojo' : ''}">${valor}<br>${simbolo}</div>
+    <div style="color: ${esRojo ? 'red' : 'black'};">${simbolo}</div>
+    <div class="esquina inferior ${esRojo ? 'rojo' : ''}">${valor}<br>${simbolo}</div>
+  `;
+
+  document.getElementById("carta").innerHTML = cartaHTML;
+}
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+window.onload = generarCarta;
